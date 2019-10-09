c8973d19c0ab7f1c506fbf98ddad392b99e2a735
love-adelar <love.adelar@gmail.com>
2019-10-10 02:29:15 +0900

    잘못된 커밋 수정

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,17 +1,13 @@
-from django.conf import settings
-from django.db import models
-from django.utils import timezone
+import pytest
+#from django.contrib.auth.models import User
+from django.test import TestCase
+from model_mommy import mommy
+from .models import Post
 
-# Create your models here.
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = modles.CharField(max_length=200)
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
+class PostTestModel(TestCase):
+   def setUp(self):
+       self.User = mommy.make(User)
+       self.post = Post(user=self.user)
+#        
+#    def test_publish(self):
+#        
--- tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-
-# Webserver run하면 200이 안 떨어지는지 확인
-class WebServerRunTests(TestCase):
-    def test_is_run(self):
-        response = self.client.get('127.0.0.1:8000')
-        self.assertEqual(response.status_code, 404)
-        self.assertNotEqual(response.status_code, 200)
